8102fcd1f71bea650a397813b0e4ffb1d51e28a2
Walter McCarthy <walter.mccarthy@apps.schoolcraft.edu>
2022-04-12 07:32:16 -0400

    Test messages after working with events

--- converter.py
+++ converter.py
@@ -1,5 +1,24 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('Text')],[sg.Button('Button')],[sg.Input()]]
+layout = [
+    [sg.Text('Text', enable_events = True,), sg.Spin(['item 1','item 2'])],
+    [sg.Button('Button', key = '-BUTTON1-')],
+    [sg.Input()],
+    [sg.Text('More Text'), sg.Button('Test Button', key = '-BUTTON2-')]
+]
 
-sg.Window('Converter',layout).read()
+window = sg.Window('Converter',layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == '-BUTTON1-':
+        print('button pressed')
+
+    if event == '-BUTTON2-':
+        print('button pressed')
+
+window.close()
